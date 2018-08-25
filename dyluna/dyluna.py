672bd504b068f5003c4ac95d9cdd7151417f7b27
gjergjk71 <gjergjk71@gmail.com>
2018-08-25 14:24:01 +0200

    Defined route decorator

--- dyluna/dyluna.py
+++ dyluna/dyluna.py
@@ -5,15 +5,24 @@ def notfound(environ,start_response):
 	return [bytes(404)]
 
 class Dyluna():
-	def __init__(self,urls):
-		self.urls = urls
-	def application(self,environ, start_response):
+	def __init__(self):
+		self.urls = []
+	def route(self,func):
+		def func_wrapper(environ,start_response,*args,**kwargs):
+			start_response("200 OK",[("Content-Type","text/html")])
+			function = func(environ,*args,**kwargs)
+			if isinstance(function, str):
+				convert = bytes(function.encode("utf-8"))
+				return [convert]
+			return function
+		return func_wrapper
+	def application(self,environ, start_response,*args,**kwargs):
 		path = environ.get("PATH_INFO","")
 		for regex,callback in self.urls:
 			if regex == path:
 				#l = int(environ.get('CONTENT_LENGTH'))
 				#print(environ['wsgi.input'].read(l))
-				return callback(environ, start_response)
+				return callback(environ, start_response,*args,**kwargs)
 		return notfound(environ,start_response)
 	def run(self,host="127.0.0.1",port=8080):
 		from paste import httpserver
